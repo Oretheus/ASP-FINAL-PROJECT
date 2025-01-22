ddb881c20cba438383e7b3592c3721a9fdd9cc94
Safiyahshaik <134588126+Safiyahshaik@users.noreply.github.com>
2025-01-22 19:15:00 +0300

    Create router.js

---
+++ router.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "@/components/Login.vue";
+import Register from "@/components/Register.vue";
+
+
+const routes = [
+    {
+      path: '/Register',
+      name: 'Register',
+      component: Register, 
+    },
+    {
+      path: '/',
+      name: 'Login',
+      component: Login, 
+    },
+  ];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
