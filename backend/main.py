2b18616636299df3a983e85810af91b2c4c6b555
Safiyahshaik <134588126+Safiyahshaik@users.noreply.github.com>
2025-02-18 16:31:52 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,7 @@ app.include_router(test_router, prefix="/v1/test", tags=["test"])
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5174",
+        "http://localhost:5173",
         "https://asp-final-project.onrender.com/",
     ],
     allow_credentials=True,
